25eadd04f3bb3d1d7489e3f09112a6ece3c00762
code-with-ammarkhan <ammarkhan.myself@gmail.com>
2024-04-13 21:02:55 +0500

    Adding a number  guessing game

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("\n\tWELCOME TO CODE WITH AMMAR - CLI NUMBER GUESSING GAME\n");
+const randomNumber = Math.floor(Math.random() * 3 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Enter Your Guess Number(Number Limit From 1 and 5):",
+    },
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("Congratulations !!! You Guess Correct Number.");
+}
+else {
+    console.log("Sorry you guessing a wrong number");
+}
